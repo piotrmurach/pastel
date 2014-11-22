7ad37d1c497b9db7df8e9853eba5e3e847c8e2f5
Piotr Murach <pmurach@gmail.com>
2014-11-22 18:50:28 +0000

    Compare dsl and decorate speeds.

--- benchmarks/speed.rb
+++ benchmarks/speed.rb
@@ -6,11 +6,16 @@ pastel = Pastel.new
 Benchmark.ips do |bench|
   bench.config(time: 5, warmup: 2)
 
-  bench.report('styles') do
-    pastel.styles
-  end
-
-  bench.report('decorate') do
+  bench.report('color decorate') do
     pastel.decorate('string', :red, :on_green, :bold)
   end
+
+  bench.report('dsl styling') do
+    pastel.red.on_green.bold('string')
+  end
+
+  bench.compare!
 end
+
+# color decorate: 14K/s
+# dsl styling:    10K/s
