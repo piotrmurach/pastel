3697341a7213b88696a39d133df69d36c0785664
Piotr Murach <pmurach@gmail.com>
2015-09-13 22:51:56 +0200

    Test immutability.

--- spec/unit/color/new_spec.rb
+++ spec/unit/color/new_spec.rb
@@ -3,6 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe Pastel::Color, '.new' do
+  it "is immutable" do
+    expect(described_class.new).to be_frozen
+  end
+
   it "allows to disable coloring" do
     color = described_class.new(enabled: false)
 
