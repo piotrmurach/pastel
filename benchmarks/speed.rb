645a3bc3ef31ca9c277e5515460a1bc3a2d2116f
Piotr Murach <pmurach@gmail.com>
2016-01-08 23:34:24 +0100

    Add benchmarks output.

--- benchmarks/speed.rb
+++ benchmarks/speed.rb
@@ -17,5 +17,14 @@ Benchmark.ips do |bench|
   bench.compare!
 end
 
-# color decorate: 14K/s
-# dsl styling:    10K/s
+# Calculating -------------------------------------
+#       color decorate      1428 i/100ms
+#          dsl styling      1174 i/100ms
+# -------------------------------------------------
+#       color decorate    16113.1 (±21.5%) i/s -      77112 in   5.054487s
+#          dsl styling    12622.9 (±20.8%) i/s -      61048 in   5.076738s
+#
+# Comparison:
+#       color decorate:    16113.1 i/s
+#          dsl styling:    12622.9 i/s - 1.28x slower
+#
